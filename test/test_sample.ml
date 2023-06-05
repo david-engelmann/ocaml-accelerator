d01d9fccd816340f861da31b84008de0dfcb1e0d
David Engelmann <david+4113@satisfilabs.com>
2023-06-05 14:24:40 -0400

    add test sample

--- test/test_sample.ml
+++ test/test_sample.ml
@@ -1,6 +1,20 @@
 (*
-open! Core
-open! Async
+open Core
+open Async
+open OUnit2
 *)
 
+let add x y = x + y
+
+let test_addition _ =
+  assert_equal 5 (add 3 3)
+
+let suite =
+    "suite" >:::
+        [
+            "test_addition" >:: test_addition;
+        ]
+
+let () =
+  run_test_tt_main suite
 
