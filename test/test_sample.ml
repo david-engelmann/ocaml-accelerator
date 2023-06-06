f5fcd1a0a35e1ce3bcbacd99ce2596940ad3d022
David Engelmann <david+4113@satisfilabs.com>
2023-06-06 16:13:02 -0400

    use ocamlformat and test string and float sample

--- test/test_sample.ml
+++ test/test_sample.ml
@@ -1,25 +1,28 @@
-(*
-open Core
-open Async
-*)
 open OUnit2
 open Ocaml_accelerator.Sample
 
 let test_addition _ =
   OUnit2.assert_equal 6 (Sample.add1 Sample.sample_x Sample.sample_y)
 
+let test_mysampletest _ =
+  match Sample.mysampletest with
+  | MySample { value } -> OUnit2.assert_equal "test" value
+
 let test_mysample4 _ =
   match Sample.mysample4 with
-   | MySample { value; } ->
-     OUnit2.assert_equal 4 value
+  | MySample { value } -> OUnit2.assert_equal 4 value
+
+let test_mysample4float _ =
+  match Sample.mysample4float with
+  | MySample { value } -> OUnit2.assert_equal 4.0 value
 
 let suite =
-    "suite" >:::
-        [
-            "test_addition" >:: test_addition;
-            "test_mysample4" >:: test_mysample4;
-        ]
+  "suite"
+  >::: [
+         "test_addition" >:: test_addition;
+         "test_mysampletest" >:: test_mysampletest;
+         "test_mysample4" >:: test_mysample4;
+         "test_mysample4float" >:: test_mysample4float;
+       ]
 
-let () =
-  run_test_tt_main suite
-
+let () = run_test_tt_main suite
